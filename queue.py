90aac9bd75620b78b950528fa86ac46d51b22c29
Pranshu Pareek <pranshupareek47@gmail.com>
2020-05-18 17:14:39 +0530

    Practised questions on Queue and Stack

--- queue.py
+++ queue.py
@@ -1,7 +1,7 @@
 # Implementing queue using array
 
 class arrayQueue:
-    def __init__(self, cap):
+    def __init__(self, cap=100):
         self.front = self.size = 0
         self.rear = cap - 1
         self.Q = [None] * cap
@@ -20,14 +20,14 @@ class arrayQueue:
         self.rear = (self.rear + 1) % self.cap
         self.Q[self.rear] = ele
         self.size = self.size + 1
-        print(ele, " is added to the Queue.")
+        # print(ele, " is added to the Queue.")
 
     def deQueue(self):
         if self.isEmpty():
             print('Empty')
             return
         ele = self.Q[self.front]
-        print(ele, " is removed from the queue.")
+        # print(ele, " is removed from the queue.")
         self.front = (self.front + 1) % self.cap
         self.size = self.size - 1
 
@@ -35,19 +35,21 @@ class arrayQueue:
         if self.isEmpty():
             print('Empty')
             return
-        print(self.Q[self.front], " is in front of queue.")
+        return self.Q[self.front]
+        # print(self.Q[self.front], " is in front of queue.")
 
     def queueRear(self):
         if self.isEmpty():
             print('Empty')
             return
-        print(self.Q[self.rear], " is in the end of queue.")
+        return self.Q[self.rear]
+        # print(self.Q[self.rear], " is in the end of queue.")
 
     def printQueue(self):
         if self.isEmpty():
-            print('Empty queue!')
+            print('Empty!')
         else:
-            print('Your queue is ', end="")
+            print('Your queue or stack is ', end="")
         for i in range(self.size):
             print(self.Q[(self.front + i) % self.cap], end="")
             if i != self.size - 1:
@@ -65,7 +67,7 @@ class node:
 
 
 class llQueue:
-    def __init__(self, cap):
+    def __init__(self, cap=float('inf')):
         self.front = None
         self.rear = None
         self.size = 0
@@ -80,28 +82,28 @@ class llQueue:
     def enQueue(self, ele):
 
         if self.isFull():
-            print('Queue is Full!!')
+            print('Full!!')
             return
         self.size = self.size + 1
         tmp = node(ele)
         if self.front is None:
             self.front = self.rear = tmp
-            print(ele, ' is added to the queue.')
+            # print(ele, ' is added to the queue.')
             return
 
         self.rear.next = tmp
         self.rear = tmp
-        print(ele, ' is added to the queue.')
+        # print(ele, ' is added to the queue.')
 
     def deQueue(self):
         if self.isEmpty():
-            print('Empty queue!!')
+            print('Empty!!')
             return
 
         self.size = self.size - 1
 
         tmp = self.front
-        print(tmp.data, ' is removed from queue.')
+        # print(tmp.data, ' is removed from queue.')
         self.front = tmp.next
 
         if self.front is None:
@@ -110,27 +112,27 @@ class llQueue:
     def queueFront(self):
 
         if self.isEmpty():
-            print('Queue is empty.')
+            print('Empty!!')
             return
-
-        print(self.front.data, ' is in the front.')
+        return self.front.data
+        # print(self.front.data, ' is in the front.')
 
     def queueRear(self):
 
         if self.isEmpty():
             print('Queue is empty.')
             return
-
-        print(self.rear.data, ' is in the last')
+        return self.rear.data
+        # print(self.rear.data, ' is in the last')
 
     def printQueue(self):
 
         if self.isEmpty():
-            print('Queue is empty.')
+            print('Empty!!')
             return
 
         temp = self.front
-        print('Your queue is ', end="")
+        print('Your queue or stack is ', end="")
         while temp.next is not None:
             print(temp.data, end=", ")
             temp = temp.next
@@ -138,29 +140,68 @@ class llQueue:
         print(temp.data, end=". \n")
 
 
-# Driver code
+# # Driver code
+#
+# # q = arrayQueue(5)
+# q = llQueue(5)
+#
+# q.enQueue(5)
+# q.enQueue(8)
+# q.enQueue(15)
+# q.enQueue(19)
+# q.queueFront()
+# q.queueRear()
+# q.printQueue()
+# q.enQueue(55)
+# q.deQueue()
+# q.deQueue()
+# q.deQueue()
+# q.deQueue()
+# q.deQueue()
+# q.deQueue()
+# q.deQueue()
+# q.deQueue()
+# q.enQueue(55)
+# q.enQueue(60)
+# q.queueFront()
+# q.queueRear()
+# q.printQueue()
 
-# q = arrayQueue(5)
-q = llQueue(5)
+# Implementing Stack using queue
 
-q.enQueue(5)
-q.enQueue(8)
-q.enQueue(15)
-q.enQueue(19)
-q.queueFront()
-q.queueRear()
-q.printQueue()
-q.enQueue(55)
-q.deQueue()
-q.deQueue()
-q.deQueue()
-q.deQueue()
-q.deQueue()
-q.deQueue()
-q.deQueue()
-q.deQueue()
-q.enQueue(55)
-q.enQueue(60)
-q.queueFront()
-q.queueRear()
-q.printQueue()
+class Stack:
+    def __init__(self, cap=100):
+        self.q1 = llQueue(cap)
+        self.q2 = llQueue(cap)
+
+    def push(self, ele):
+        self.q2.enQueue(ele)
+        while not self.q1.isEmpty():
+            self.q2.enQueue(self.q1.queueFront())
+            self.q1.deQueue()
+        [self.q1, self.q2] = [self.q2, self.q1]
+
+
+    def pop(self):
+        self.q1.deQueue()
+
+    def print(self):
+        self.q1.printQueue()
+
+
+stack = Stack()
+
+stack.push(5)
+stack.push(7)
+stack.push(8)
+stack.push(15)
+stack.push(19)
+stack.push(25)
+stack.print()
+print('push done')
+
+stack.pop()
+stack.pop()
+stack.pop()
+
+stack.print()
--- stack.py
+++ stack.py
@@ -1,7 +1,10 @@
+# Implementation using LinkedList
+
 class StackNode:
     def __init__(self, data):
         self.data = data
         self.next = None
+
 
 class Stack:
 
@@ -11,11 +14,11 @@ class Stack:
     def isEmpty(self):
         return True if self.root is None else False
 
-    def push(self,data):
+    def push(self, data):
         newNode = StackNode(data)
         newNode.next = self.root
         self.root = newNode
-        print(data, " is pushed to stack.")
+        # print(data, " is pushed to stack.")
 
     def pop(self):
         if self.isEmpty():
@@ -25,19 +28,30 @@ class Stack:
         popped = temp.data
         return popped
 
+    def printStack(self):
+        if self.isEmpty():
+            print('Empty')
+            return
+        print('Stack is ', end="")
+        temp = self.root
+        while temp.next is not None:
+            print(temp.data, end=" ")
+            temp = temp.next
+
     def peek(self):
         if self.isEmpty():
-            return float("-inf")
+            return -1
         return self.root.data
 
-st = Stack()
 
-st.push(10)
-st.push(15)
-st.push(20)
-
-print(st.pop(), " is popped out")
-print(st.peek()," is peek of stack")
+# st = Stack()
+#
+# st.push(10)
+# st.push(15)
+# st.push(20)
+#
+# print(st.pop(), " is popped out")
+# print(st.peek()," is peek of stack")
 
 
 # # Implementing with array
@@ -76,3 +90,232 @@ print(st.peek()," is peek of stack")
 # print(pop(st),' popped')
 # print(pop(st),' popped')
 
+# Check for balanced parenthesis
+def checkBalance(str):
+    stack = Stack()
+    ob = ['{', '[', '(', '<']
+    cb = ['}', ']', ')', '>']
+
+    isBalanced = False
+    for i in range(len(str)):
+        if str[i] in ob:
+            stack.push(ob.index(str[i]))
+        else:
+            p = stack.pop()
+            if p == cb.index(str[i]):
+                isBalanced = True
+                continue
+            else:
+                isBalanced = False
+                break
+
+    if not stack.isEmpty():
+        isBalanced = False
+    if isBalanced:
+        print('String', str, 'is Balanced.')
+    else:
+        print('String', str, 'is not balanced')
+
+
+# str = input()
+#
+# checkBalance(str)
+
+
+# Implementing two stack in an array
+
+class twostack:
+    def __init__(self, cap=10):
+        self.arr = [None] * cap
+        self.top1 = -1
+        self.top2 = cap
+        self.cap = cap
+
+    def push1(self, ele):
+        if not self.top1 + 1 < self.top2:
+            print('Overflow!!')
+            return
+        self.top1 = self.top1 + 1
+        self.arr[self.top1] = ele
+
+    def push2(self, ele):
+        if not self.top1 + 1 < self.top2:
+            print('Overflow!!')
+            return
+        self.top2 = self.top2 - 1
+        self.arr[self.top2] = ele
+
+    def pop1(self):
+        if self.top1 == -1:
+            print('Underflow!!')
+            return
+        ele = self.arr[self.top1]
+        self.top1 = self.top1 - 1
+        return ele
+
+    def pop2(self):
+        if self.top2 == self.cap:
+            print('Underflow!!')
+            return
+        ele = self.arr[self.top2]
+        self.top2 = self.top2 + 1
+        return ele
+
+    def print1(self):
+        if self.top1 == -1:
+            print('Underflow!!')
+            return
+        for i in range(self.top1):
+            print(self.arr[i], end=", ")
+        print(self.arr[self.top1], end=".\n")
+
+    def print2(self):
+        if self.top2 == self.cap:
+            print('Underflow!!')
+            return
+        for i in range(self.cap - 1, self.top2, -1):
+            print(self.arr[i], end=", ")
+        print(self.arr[self.top2], end=".\n")
+
+
+# cs = twostack(5)
+# cs.push1(10)
+# cs.push1(15)
+# cs.push1(13)
+# cs.pop1()
+# cs.push2(3)
+# cs.push2(9)
+# cs.print1()
+# cs.print2()
+
+
+# Implementing k stacks in an array of length n
+
+class kStack:
+    def __init__(self, k, n):
+        self.k = k
+        self.n = n
+        self.arr = [None] * self.n
+        self.top = [-1] * self.k
+        self.free = 0
+        self.next = [i + 1 for i in range(self.n)]
+        self.next[self.n - 1] = -1
+
+    def isEmpty(self, sn):
+        return self.top[sn] == -1
+
+    def isFull(self):
+        return self.free == -1
+
+    def push(self, item, sn):
+        if self.isFull():
+            print('Full!!')
+            return
+
+        insert_at = self.free
+        self.free = self.next[self.free]
+        self.arr[insert_at] = item
+        self.next[insert_at] = self.top[sn]
+        self.top[sn] = insert_at
+
+    def pop(self, sn):
+        if self.isEmpty(sn):
+            print('Empty!!')
+            return
+
+        popped = self.top[sn]
+        self.top[sn] = self.next[popped]
+        self.next[popped] = self.free
+        self.free = popped
+        return self.arr[popped]
+
+    def printStack(self, sn):
+        if self.isEmpty(sn):
+            print('Empty!!')
+            return
+
+        temp = self.top[sn]
+        while temp != -1:
+            print(self.arr[temp], end=" ")
+            temp = self.next[temp]
+
+
+# # Driver code
+#
+# kstacks = kStack(3, 10)
+#
+# # Push some items onto stack number 2.
+# kstacks.push(15, 2)
+# kstacks.push(45, 2)
+#
+# # Push some items onto stack number 1.
+# kstacks.push(17, 1)
+# kstacks.push(49, 1)
+# kstacks.push(39, 1)
+#
+# # Push some items onto stack number 0.
+# kstacks.push(11, 0)
+# kstacks.push(9, 0)
+# kstacks.push(7, 0)
+#
+# print("Popped element from stack 2 is " +
+#       str(kstacks.pop(2)))
+# print("Popped element from stack 1 is " +
+#       str(kstacks.pop(1)))
+# print("Popped element from stack 0 is " +
+#       str(kstacks.pop(0)))
+#
+# kstacks.printStack(0)
+
+
+# Stock market problem (Nearest greater element on the left)
+
+def stockM(arr):
+    s = Stack()
+    s.push(0)
+    sc = [1]
+
+    for i in range(1, len(arr)):
+        sp = s.peek()
+        while not s.isEmpty() and arr[i] > arr[s.peek()]:
+            s.pop()
+            sp = s.peek()
+        s.push(i)
+        sc.append(i - sp)
+    return sc
+
+
+# # arr = [10, 5, 2, 7, 18, 15, 16, 17]
+# # arr = [10, 20 , 30]
+# arr = [3, 2, 1]
+# print(stockM(arr))
+
+
+# Nearest greater element in right
+
+def greaterInRight(arr):
+    s = Stack()
+    s.push(0)
+    n = len(arr)
+    gr = [None] * n
+    for i in range(1, n):
+        sp = s.peek()
+        if arr[i] < arr[sp]:
+            s.push(i)
+            continue
+        while arr[i] > arr[s.peek()]:
+            sp = s.pop()
+            gr[sp] = arr[i]
+        s.push(i)
+
+    while not s.isEmpty():
+        sp = s.pop()
+        gr[sp] = -1
+
+    return gr
+
+
+# arr = [5, 10, 7, 8, 3, 2, 12]
+# arr = [10,20,30]
+arr = [30, 20, 10]
+print(greaterInRight(arr))
